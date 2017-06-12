8325bda289496d55c652752a239130019098aac7
s.sriram <srisd@hotmail.com>
2017-06-12 20:50:24 +0530

    reviews scaffold

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :movies
+                  has_many :reviews, dependent: :destroy
+
 end
